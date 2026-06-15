9d5d8547d3b633c40d6a617552ff1eb0ca6cb183
Yash-Sharma-Programer <yashbca029@gmail.com>
2026-06-15 14:14:43 +0530

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import app from './src/app.js'
 import ConnectDB from './src/config/database.js'
 
 ConnectDB()
+PORT = process.env.PORT
 app.listen(3000, () => {
     console.log("App Listening On Port: 3000")
 })
