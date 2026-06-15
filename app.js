1063678edba06ac719f4f0edded7efec595bae53
Yash-Sharma-Programer <yashbca029@gmail.com>
2026-06-15 18:27:53 +0530

    qssq

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
-import authRouter from '../src/routes/auth.route.js'
-import cartRouter from '../src/routes/cart.route.js'
-import orderRouter from '../src/routes/order.route.js'
+import authRouter from './src/routes/auth.route.js'
+import cartRouter from './src/routes/cart.route.js'
+import orderRouter from './src/routes/order.route.js'
 import cookieParser from "cookie-parser"
 import cors from 'cors'
 
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://inter-ecom-front-d2y7.vercel.app/",
+    origin: "https://inter-ecom-front-d2y7.vercel.app",
     credentials: true
 }));
 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import app from './src/app.js'
+import app from './app.js'
 import ConnectDB from './src/config/database.js'
 import config from './src/config/config.js'
 const PORT = config.PORT
